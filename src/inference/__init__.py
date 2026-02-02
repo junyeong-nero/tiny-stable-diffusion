5720550df725541b86fb73103f6793afc5df638e
junyeong-nero <magicwho@unist.ac.kr>
2026-02-02 21:15:42 +0900

    feat: add AnimationGenerator for GIF inference

--- src/inference/__init__.py
+++ src/inference/__init__.py
@@ -2,10 +2,18 @@
 
 from src.inference.generator import demo, generate
 from src.inference.vae_inference import reconstruct_vae, reconstruct_vae_batch
+from src.inference.animation_generator import (
+    AnimationGenerator,
+    generate_animation,
+    animation_demo,
+)
 
 __all__ = [
     "generate",
     "demo",
     "reconstruct_vae",
     "reconstruct_vae_batch",
+    "AnimationGenerator",
+    "generate_animation",
+    "animation_demo",
 ]
